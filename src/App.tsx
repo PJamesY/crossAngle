5dd5c5defed16876ce13143e13bc37816af5eced
james <junyoungcat@gmail.com>
2022-03-18 23:24:01 +0900

    Set Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import SurveyPage from "./pages/SurveyPage";
+import WelcomePage from "./pages/WelcomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/survey" element={<SurveyPage />} />
+        <Route path="/welcome" element={<WelcomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
