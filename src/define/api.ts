c8ee0e22fecc5a63b2383781bbc2f207923e00db
james <junyoungcat@gmail.com>
2022-03-20 20:08:48 +0900

    Define api name

---
+++ src/define/api.ts
@@ -0,0 +1,7 @@
+export const BASE_URL = "http://dev.crossAngle.com";
+
+export const END_POINT = {
+  LANDING_PAGE_MSG: "landing_page_msg",
+  SURVEY_PAGE_QUESTION: "survey_page_question",
+  WELCOME_PAGE_MSG: "welcome_page_msg",
+};
