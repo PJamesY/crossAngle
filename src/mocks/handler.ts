1396eb8ffde8ffa04c58e1d2d82e2602a56ab8ac
james <junyoungcat@gmail.com>
2022-03-20 20:10:11 +0900

    Apply Define to handler api

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,13 +1,34 @@
 import { rest } from "msw";
-import dotenv from "dotenv";
-dotenv.config();
+import { BASE_URL, END_POINT } from "../define/api";
+// import dotenv from "dotenv";
+// dotenv.config();
 
 const handlers = [
-  rest.get(`${process.env.BASE_URL}/helloMsg.json`, (req, res, ctx) => {
+  // 렌딩 페이지 mock api
+  rest.get(`${BASE_URL}/${END_POINT.LANDING_PAGE_MSG}`, (_, res, ctx) => {
     return res(ctx.json({ data: { msg: "크로스 앵글 랜딩페이지" } }));
   }),
-  rest.get(`${process.env.BASE_URL}/WelcomMsg.json`, (req, res, ctx) => {
+  // 환영 페이지 mock api
+  rest.get(`${BASE_URL}/${END_POINT.WELCOME_PAGE_MSG}`, (_, res, ctx) => {
     return res(ctx.json({ data: { msg: "환영합니다 크로스 앵글입니다." } }));
+  }),
+  // 설문 페이지 mock api
+  rest.get(`${BASE_URL}/${END_POINT.SURVEY_PAGE_QUESTION}`, (_, res, ctx) => {
+    const idx = Math.round(Math.random()); // 0 또는 1
+    const datas = [
+      { type: "ox", surveyMsg: "크로스앵글을 찾으신 경로가 블로그인가요?" },
+      {
+        type: "text",
+        surveyMsg: "크로스앵글 피드백을 남겨주세요",
+      },
+    ];
+    const data = datas[idx];
+
+    return res(
+      ctx.json({
+        data,
+      })
+    );
   }),
 ];
 
