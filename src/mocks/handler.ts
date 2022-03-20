a32a55e998e57a7584b134494608bfe3b3f98b4e
james <junyoungcat@gmail.com>
2022-03-20 16:58:31 +0900

    Add Welcome msg api mock test case

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -6,6 +6,9 @@ const handlers = [
   rest.get(`${process.env.BASE_URL}/helloMsg.json`, (req, res, ctx) => {
     return res(ctx.json({ data: { msg: "크로스 앵글 랜딩페이지" } }));
   }),
+  rest.get(`${process.env.BASE_URL}/WelcomMsg.json`, (req, res, ctx) => {
+    return res(ctx.json({ data: { msg: "환영합니다 크로스 앵글입니다." } }));
+  }),
 ];
 
 export default handlers;
